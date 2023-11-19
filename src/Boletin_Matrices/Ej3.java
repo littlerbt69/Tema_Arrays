05463258b5cf0404082a648e3756fa6a72511726
roberto <robertocd509@gmail.com>
2023-11-20 00:56:14 +0100

    cambios dia 16/11

---
+++ src/Boletin_Matrices/Ej3.java
@@ -0,0 +1,22 @@
+package Boletin_Matrices;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        imprimeMatrizLinea(matriz);
+
+    }
+
+    public static void imprimeMatrizLinea (int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+        }
+    }
+}
